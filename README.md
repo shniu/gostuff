02951f223a1148e8e2679e2c52c6be647a806ba8
shniu <xiaoniu198903@126.com>
2019-03-09 16:50:27 +0800

    Modify to Restful API put /api/cache/

--- README.md
+++ README.md
@@ -10,13 +10,12 @@ An implement of Distributed cache
 curl -v http://localhost:5000
 
 # Test: add cache
-curl -X POST -H 'Content-Type: application/json' -d '{"key":"uid","value":"1009873"}' http://localhost:5000/api/cache
+curl -v -X PUT -d "bbbbbbbbbbbwwww" http://localhost:5000/api/cache/u_1234
 # Response should be: 
 # Response 200
 # {"code":0,"message":"succeed"} 
 
-curl -X POST -H 'Content-Type: application/json' -d '{"key":"","value":"1009873"}' http://localhost:5000/api/cache
+curl -v -X PUT -d '{"key":"","value":"1009873"}' http://localhost:5000/api/cache/
 # Response should be:
-# Response 200
-# {"code":403,"message":"Null keys are forbidden"}
+# Response 400
 ```
--- server/server.go
+++ server/server.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func handleCache(w http.ResponseWriter, req *http.Request) {
 
-	if req.Method == "POST" {
+	path := req.URL.EscapedPath()
+	key := strings.Split(path, "/")[3]
+	fmt.Println("URL Path: ", path, key)
+
+	if len(key) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if req.Method == http.MethodPut {
 		body, _ := ioutil.ReadAll(req.Body)
 		fmt.Println("body:", string(body))
 		_, _ = w.Write([]byte("{\"code\":0,\"message\":\"succeed\"}"))
@@ -19,6 +29,6 @@ func handleCache(w http.ResponseWriter, req *http.Request) {
 
 func CreateCacheServer() {
 	serveMux := http.NewServeMux()
-	serveMux.HandleFunc("/api/cache", handleCache)
+	serveMux.HandleFunc("/api/cache/", handleCache)
 	http.ListenAndServe(":5000", serveMux)
 }
