f314a8338a04bf0c67b1e54db73e396c00a46271
shniu <niushaohan@digcredit.com>
2019-10-08 16:54:30 +0800

    ringbuffer fill count 实现

---
+++ leetcode/ringbuffer_test.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+)
+
+
+func TestRingBuffer(t *testing.T) {
+	rb := NewRingBuffer(5)
+	rb.Put(1)
+	rb.Put(30)
+	rb.Put(2)
+	rb.Put(33)
+	rb.Put(40)
+
+	v, ok := rb.Take()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, 1, v)
+	fmt.Println(rb)
+
+	v, ok = rb.Take()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, 30, v)
+	fmt.Println(rb)
+
+	v, ok = rb.Take()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, 2, v)
+	fmt.Println(rb)
+
+	rb.Put(100)
+	rb.Take()
+	rb.Take()
+	v, ok = rb.Take()
+	assert.Equal(t, true, ok)
+	assert.Equal(t, 100, v)
+	fmt.Println(rb)
+
+	v, ok = rb.Take()
+	assert.Equal(t, false, ok)
+	assert.Equal(t, -1, v)
+	fmt.Println(rb)
+}
